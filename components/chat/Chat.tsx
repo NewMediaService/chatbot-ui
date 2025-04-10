37c8f47069d265d5364dd0b35329af641621d7c3
LuHKaeNMS <101269569+LuHKaeNMS@users.noreply.github.com>
2025-04-10 22:12:52 +0200

    init nms patch1

---
+++ components/chat/Chat.tsx
@@ -0,0 +1,3 @@
+export default function Chat() {
+  return <div>Chat-Komponente erfolgreich geladen.</div>;
+}
---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Chat from "@components/chat/Chat";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>NMSGPT</title>
+      </Head>
+      <main>
+        <Chat />
+      </main>
+    </>
+  );
+}
