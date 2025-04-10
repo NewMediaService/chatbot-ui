9cd7f342bfa29e03cc53d77fd8f14b9dd1b776ce
LuHKaeNMS <101269569+LuHKaeNMS@users.noreply.github.com>
2025-04-10 22:33:30 +0200

    fix 4 init nms

--- next.config.js
+++ next.config.js
@@ -1,1 +1,18 @@
-module.exports = { reactStrictMode: true };
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    if (!config.resolve.plugins) {
+      config.resolve.plugins = [];
+    }
+
+    config.resolve.plugins.push(
+      new TsconfigPathsPlugin({
+        configFile: './tsconfig.json',
+      })
+    );
+
+    return config;
+  },
+};
